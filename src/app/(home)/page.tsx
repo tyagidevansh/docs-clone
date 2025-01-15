87c97fce7eba16737c32540c7eb4e95f96b7af92
tyagidevansh <devanshtyagi0607@gmail.com>
2025-01-15 23:01:36 +0530

    templates

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,14 @@
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const DocumentsPage = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
+      </div>
+      <div className="mt-16">
+        <TemplatesGallery /> 
       </div>
     </div>
   )
---
+++ src/app/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  { 
+    id: "blank", 
+    label: "Blank document", 
+    imageUrl: "/blank-document.svg" 
+  },
+  { 
+    id: "business-letter", 
+    label: "Business letter", 
+    imageUrl: "/business-letter.svg" 
+  },
+  { 
+    id: "cover-letter", 
+    label: "Cover letter", 
+    imageUrl: "/cover-letter.svg" 
+  },
+  { 
+    id: "letter", 
+    label: "Letter", 
+    imageUrl: "/letter.svg" 
+  },
+  { 
+    id: "project-proposal", 
+    label: "Project proposal", 
+    imageUrl: "/project-proposal.svg" 
+  },
+  { 
+    id: "resume", 
+    label: "Resume", 
+    imageUrl: "/resume.svg" 
+  },
+  { 
+    id: "software-proposal", 
+    label: "Software proposal", 
+    imageUrl: "/software-proposal.svg" 
+  },
+]
